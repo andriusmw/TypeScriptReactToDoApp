22c5ff2eec29e0b4b83730c6a70a9581edf9dc1c
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-02-29 08:51:34 +0100

    creating a header + using an image on the component

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,7 +1,12 @@
 import CourseGoal from "./components/CourseGoal.tsx";
+import goalsImg from "./assets/images/goalsImg.jpg"
+import Header from "./components/Header.tsx";
 
 export default function App(){
   return <main>
+    <Header image={{src: goalsImg, alt:"a list of goals"}}>
+      <h1>Your course Goals</h1>
+    </Header>
     <CourseGoal 
       title="Learn React" 
       description="using TypeScript"
